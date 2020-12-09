5bf0a1d45a201a9cb84edbdf232710efbf59d10a
gandatadyo <gandatadyosurya@yahoo.com>
2020-12-09 12:11:18 +0700

    update app

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-app.use(express.static(path.join(__dirname, 'build')));
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
